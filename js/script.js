afee9de60a9c73a0525ee262186f3d75816e51b1
Artsemi Shyshkin <artsemi.shyshkin@gmail.com>
2022-03-25 22:23:35 +0300

    Send data to server using Fetch API

--- js/script.js
+++ js/script.js
@@ -244,10 +244,6 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-Type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -255,19 +251,22 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(object),
+            })
+            .then(data => data.text())
+            .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+                    statusMessage.remove();   
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
